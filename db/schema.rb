62f3583af557b5d2bbc15da30959eae53a0efe71
Zack Siri <zack@codemy.net>
2016-10-26 02:15:31 +0700

    [#44] Added stateful enum for published post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808184412) do
+ActiveRecord::Schema.define(version: 20160823181047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 20160808184412) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "status",       default: 0
+    t.datetime "published_at"
   end
 
   create_table "taggings", force: :cascade do |t|
