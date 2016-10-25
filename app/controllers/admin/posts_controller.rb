0b5f344db5ea5d57c66c31c6b82e94252e4a1a71
Zack Siri <zack@codemy.net>
2016-10-26 03:33:20 +0700

    added publish / un-publish button for posts

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -23,7 +23,7 @@ class Admin::PostsController < Admin::BaseController
   def update
     @post = Post.where(id: params[:id]).first
     if @post.update_attributes(post_params)
-      redirect_to posts_path
+      redirect_to admin_posts_path
     else
       # render the edit form again
     end
@@ -41,6 +41,6 @@ class Admin::PostsController < Admin::BaseController
 private
 
   def post_params
-    params.require(:post).permit(:title, :body, :tag_ids => [])
+    params.require(:post).permit(:state_event, :title, :body, :tag_ids => [])
   end
 end
