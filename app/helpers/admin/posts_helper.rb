036f879ad3f436d6ed18e25a3cea309b535fc6df
Zack Siri <zack@codemy.net>
2016-11-09 04:47:16 +0700

    [#50] Generating enum filter hash

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -3,11 +3,25 @@ module Admin
     class << self
       def render_states(view)
         view.sanitize(
-          Post.statuses.keys.map do |status|
+          statuses(view).map do |status, path|
             view.content_tag :li do
-              view.link_to status, view.by_status_admin_posts_path(status)
+              view.link_to status, path
             end
           end.join
+        )
+      end
+
+      private
+
+      def statuses(view)
+        # { 'all' => admin_posts_path,
+        #   'draft' => by_status_admin_posts_path('draft'),
+        #   'published' => by_status_admin_posts_path('published') }
+
+        { 'all' => view.admin_posts_path }.merge(
+          Post.statuses.keys.inject({}) do |hash, status|
+            hash.merge(status => view.by_status_admin_posts_path(status))
+          end
         )
       end
     end
